88b1e9e974dc5f6bed42688ac86029da85175026
Abdulsametileri <sametileri07@gmail.com>
2022-07-11 08:44:20 +0300

    feat: implement stomp connect directly queue on ws

---
+++ stomp/cc_stomp.js
@@ -0,0 +1,22 @@
+let stompClient;
+
+const stompConfig = {
+    connectHeaders: {
+        login: "guest",
+        passcode: "guest",
+    },
+    brokerURL: "ws://localhost:15674/ws",
+    debug: function (str) {
+        console.log('STOMP: ' + str);
+    },
+    reconnectDelay: 200,
+    onConnect: function (frame) {
+        const subscription =stompClient.subscribe('/queue/taxi_information', function (message) {
+            const body = JSON.parse(message.body);
+            const latitude = body.latitude;
+            const longitude = body.longitude;
+            console.log(body);
+        });
+    }};
+stompClient = new StompJs.Client(stompConfig);
+stompClient.activate();
